347ff2938e0996c6f86ac4d5d088d058f2d9dbd3
Simon Johann Skødt <simon@skodt.dk>
2021-11-15 17:38:29 +0100

    Getting the yaml file to work in this project

--- ThesisBank/ThesisBank/Data/Core/ITeacherRepository.cs
+++ ThesisBank/ThesisBank/Data/Core/ITeacherRepository.cs
@@ -1,13 +1,15 @@
-﻿namespace ThesisBank.Data
+﻿using ThesisBank.Data;
+
+namespace Core
 {
     public interface ITeacherRepository
     {
         Response Accecpt(int thesisID){
             throw new NotImplementedException();
-        };
+        }
 
         Response Reject(int thesisID){
             throw new NotImplementedException();
-        };
+        }
     }
 }
