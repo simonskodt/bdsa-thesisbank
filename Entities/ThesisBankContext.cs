14b519a46ab0b0174b1ebde0bbb56fff13021845
Victor Brorson <victorbrorson@Victors-MacBook-Pro.local>
2021-11-17 18:28:18 +0100

    Almost ready to migrate

--- Entities/ThesisBankContext.cs
+++ Entities/ThesisBankContext.cs
@@ -1,15 +1,17 @@
 ﻿namespace Entities;
-public class ThesisBankContext : DbContext {
+public class ThesisBankContext : DbContext 
+{
     public DbSet<Student> Students { get; set; }
     public DbSet<Teacher> Teachers { get; set; }
     public DbSet<Thesis> Theses { get; set; }
 
+    public ThesisBankContext(){}
     public ThesisBankContext(DbContextOptions<ThesisBankContext> options) : base(options)
     {
         
     }
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-
+        
     }
 }
--- ThesisBank/Program.cs
+++ ThesisBank/Program.cs
@@ -1,6 +1,8 @@
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Entities;
 using ThesisBank.Data;
+using Microsoft.EntityFrameworkCore;
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,3 +31,26 @@ app.MapBlazorHub();
 app.MapFallbackToPage("/_Host");
 
 app.Run();
+
+static IConfiguration LoadConfiguration()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddUserSecrets<Program>();
+
+        return builder.Build();
+    }
+
+    static void Main(string[] args)
+    {
+        var configuration = LoadConfiguration();
+        //var connectionString = "Server=localhost;Database=MyProject;User Id=sa;Password=37d6661e-6894-4944-88fc-e07256e30c81";
+        //var connectionString = "Server=localhost;Database=$database;User Id=sa;Password=$password"; //(retrieve password)
+        var connectionString = configuration.GetConnectionString("ThesisBank");
+        //using var connection = new SqlConnection(connectionString);
+        var optionsBuilder = new DbContextOptionsBuilder<ThesisBankContext>().UseSqlServer(connectionString);
+        //var optionsBuilder = new DbContextOptionsBuilder<KanbanContext>().UseNpgsql(connectionString); //POSTGRES
+        using var context = new ThesisBankContext(optionsBuilder.Options);
+        
+    }
