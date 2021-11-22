dcd60e6ee7065659abcd463ecd73a9f9af3db97f
Victor Brorson <victorbrorson@Victors-MacBook-Pro.local>
2021-11-22 15:43:27 +0100

    Finishes initial set up before starting tests

--- Core/ITeacherRepository.cs
+++ Core/ITeacherRepository.cs
@@ -1,6 +1,10 @@
-namespace Core;
+﻿namespace Core;
 
 public interface ITeacherRepository
 {
+
+    public Response Accept(int ThesisID);
+
+    public Response Reject(int ThesisID);
     
 }
---
+++ Entities.Tests/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Entities;
+global using System;
+global using Xunit;
+global using Microsoft.Data.Sqlite;
+global using Microsoft.EntityFrameworkCore;
+global using System.Collections.Generic;
