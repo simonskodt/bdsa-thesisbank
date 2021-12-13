6424a4fcfc7dc1770e0aa264f02a9b6e0e857389
Alyson De Souza <alysondsouza@yahoo.com>
2021-12-13 17:26:25 +0100

    a

--- run_1.ps1
+++ run_1.ps1
@@ -1,10 +1,13 @@
-mkdir .\.local
-echo '*' > .\.local\.gitignore
+mkdir ..local
+echo '*' > ..local.gitignore
 $pw = New-Guid
-echo "Server=db;Database=ThesisBank;User Id=sa;Password=$pw" > .\.local\connection_string.txt
+$connectionString = "Server=db;Database=ThesisBank;User Id=sa;Password=$pw"
+echo $connectionString > ..local\connection_string.txt
 Set-Clipboard -Value "Server=localhost;Database=ThesisBank;User Id=sa;Password=$pw" 
-echo $pw > .\.local\db_password.txt
-cd Entities
+echo $pw > ..local\db_password.txt
+cd .\Server
+dotnet user-secrets init
+dotnet user-secrets set "ConnectionStrings:ThesisBank" "$connectionString"
+cd ..\Entities\
 dotnet ef migrations add InitialMigration -s ..\Server\
 cd ..
-
