86b189bdd2a7454279745ec0375f70b63a0f4d2a
Leonoratheoret <70759304+Leonoratheoret@users.noreply.github.com>
2021-12-15 15:15:48 +0100

    working on Leo's mac

--- run_mac_1.ps1
+++ run_mac_1.ps1
@@ -5,9 +5,9 @@ $connectionString = "Server=db;Database=ThesisBank;User Id=sa;Password=$pw"
 echo "Server=db;Database=ThesisBank;User Id=sa;Password=$pw" > ./.local/connection_string.txt
 Set-Clipboard -Value "Server=localhost;Database=ThesisBank;User Id=sa;Password=$pw" 
 echo $pw > ./.local/db_password.txt
-cd ./.Server
+cd ./Server
 dotnet user-secrets init
 dotnet user-secrets set "ConnectionStrings:ThesisBank" "$connectionString"
-cd ./.Entities
+cd ../Entities
 dotnet ef migrations add InitialMigration -s ../Server/
 cd ..
