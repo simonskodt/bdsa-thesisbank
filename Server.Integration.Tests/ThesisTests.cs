34c7074f6e5b3aac36f015fbbe6999e6c61a7b77
Ahmed Bassam Galal <ahga@itu.dk>
2021-12-19 14:01:20 +0100

    KEK

--- Server.Integration.Tests/ThesisTests.cs
+++ Server.Integration.Tests/ThesisTests.cs
@@ -1,4 +1,4 @@
-/* namespace Server.Integration.Tests;
+ namespace Server.Integration.Tests;
 
 // Credit to Rasmus Lystrøm for providing an integration test structure, which is used in this directory
 
@@ -16,16 +16,16 @@ public class ThesisTests : IClassFixture<CustomWebApplicationFactory>
     [Fact]
     public async Task Get_Returns_theses()
     {
-        var theses = await _client.GetFromJsonAsync<ThesisDTO[]>("api/Thesis");
+        var theses = await _client.GetFromJsonAsync<ThesisDTO[]>("api/theses/1");
 
         Assert.NotNull(theses);
-        Assert.False(theses.Length < 3);
+        Assert.False(theses.Length > 3);
     }
 
     [Fact]
     public async Task Get_Returns_correct_Thesis_from_name()
     {
-        var theses = await _client.GetFromJsonAsync<MinimalThesisDTO[]>("api/Thesis");
+        var theses = await _client.GetFromJsonAsync<MinimalThesisDTO[]>("api/theses/1");
 
         Assert.Equal("A study on why notepad is the best IDE", theses[theses.Length - 1].Name);
         Assert.DoesNotContain(theses, t => t.Name == ".NET framework");
@@ -34,18 +34,7 @@ public class ThesisTests : IClassFixture<CustomWebApplicationFactory>
     [Fact]
     public async Task Get_Returns_correct_Teacher_from_Thesis()
     {
-        var theses = await _client.GetFromJsonAsync<MinimalThesisDTO[]>("api/Thesis");
-
-        // Assert.Contains(theses, t => t.Teacher.Name == "Thore");
+        var theses = await _client.GetFromJsonAsync<MinimalThesisDTO[]>("api/theses/1");
         Assert.Equal("Thore", theses[theses.Length - 1].TeacherName);
-        Assert.DoesNotContain(theses, t => t.Name == ".NET framework");
     }
-
-    [Fact]
-    public async Task Get_Returns_description()
-    {
-        var theses = await _client.GetFromJsonAsync<MinimalThesisDTO[]>("api/Thesis");
-
-        Assert.NotNull(theses[0].Description);
-    }
-} */
+} 
