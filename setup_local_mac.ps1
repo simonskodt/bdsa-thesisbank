a217a84b1c080b5d5ad4b12ff6ea3c62a21dbee1
Ahmed Bassam Galal <74544573+SgTTrollinator@users.noreply.github.com>
2021-12-21 16:05:03 +0100

    Delete setup_local_mac.ps1

--- setup_local_mac.ps1
+++
@@ -1,6 +0,0 @@
-mkdir ./.local
-echo '*' > ./.local/.gitignore
-$pw = New-Guid
-echo "Server=db;Database=ThesisBank;User Id=sa;Password=$pw" > ./.local/connection_string.txt
-Set-Clipboard -Value "Server=localhost;Database=ThesisBank;User Id=sa;Password=$pw" 
-echo $pw > ./.local/db_password.txt
